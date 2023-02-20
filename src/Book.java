02445af23504fbd3ccd8c3bc2771ca710cd83784
adriangajda96 <adrian.gajda.kontakt@gmail.com>
2023-02-20 16:01:46 +0100

    v0.3

--- src/Book.java
+++ src/Book.java
@@ -7,4 +7,18 @@ public class Book {
     String publisher;
     String isbn;
 
+    Book(String bookTitle, String bookAuthor, int bookRelease, int bookPages, String bookPublisher, String bookIsbn) {
+        title = bookTitle;
+        author = bookAuthor;
+        releaseYear = bookRelease;
+        pages = bookPages;
+        publisher = bookPublisher;
+        isbn = bookIsbn;
+    }
+
+    void printInfo() {
+        String info = title + "; " + author + "; " + releaseYear + "; " + pages + "; " + publisher + "; " + isbn;
+        System.out.println(info);
+    }
+
 }
